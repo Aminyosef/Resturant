3ecbb5bd20c0a8929bbf4aad1ffe184ec9cf5e41
AminGomaa <amingomaatube@gmail.com>
2022-12-13 23:37:32 +0200

    main form

--- Resturant/Forms/Form1.Designer.cs
+++ Resturant/Forms/Form1.Designer.cs
@@ -28,39 +28,226 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pnlMenu = new System.Windows.Forms.Panel();
+            this.btnOption = new System.Windows.Forms.Button();
+            this.btnReport = new System.Windows.Forms.Button();
+            this.btnSetup = new System.Windows.Forms.Button();
+            this.btnPOS = new System.Windows.Forms.Button();
             this.pnlLogo = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlTitl = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblItem = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pnlMain = new System.Windows.Forms.Panel();
             this.pnlMenu.SuspendLayout();
+            this.pnlLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlTitl.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlMenu
             // 
             this.pnlMenu.BackColor = System.Drawing.Color.Silver;
+            this.pnlMenu.Controls.Add(this.btnOption);
+            this.pnlMenu.Controls.Add(this.btnReport);
+            this.pnlMenu.Controls.Add(this.btnSetup);
+            this.pnlMenu.Controls.Add(this.btnPOS);
             this.pnlMenu.Controls.Add(this.pnlLogo);
             this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlMenu.Location = new System.Drawing.Point(0, 0);
             this.pnlMenu.Name = "pnlMenu";
-            this.pnlMenu.Size = new System.Drawing.Size(200, 450);
+            this.pnlMenu.Size = new System.Drawing.Size(192, 592);
             this.pnlMenu.TabIndex = 0;
+            // 
+            // btnOption
+            // 
+            this.btnOption.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnOption.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnOption.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOption.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnOption.FlatAppearance.BorderSize = 0;
+            this.btnOption.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOption.ForeColor = System.Drawing.Color.Black;
+            this.btnOption.Image = ((System.Drawing.Image)(resources.GetObject("btnOption.Image")));
+            this.btnOption.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOption.Location = new System.Drawing.Point(0, 354);
+            this.btnOption.Name = "btnOption";
+            this.btnOption.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnOption.Size = new System.Drawing.Size(192, 82);
+            this.btnOption.TabIndex = 4;
+            this.btnOption.Text = "Options";
+            this.btnOption.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnOption.UseVisualStyleBackColor = false;
+            this.btnOption.Click += new System.EventHandler(this.btnOption_Click);
+            // 
+            // btnReport
+            // 
+            this.btnReport.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnReport.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnReport.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReport.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReport.FlatAppearance.BorderSize = 0;
+            this.btnReport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReport.ForeColor = System.Drawing.Color.Black;
+            this.btnReport.Image = ((System.Drawing.Image)(resources.GetObject("btnReport.Image")));
+            this.btnReport.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReport.Location = new System.Drawing.Point(0, 272);
+            this.btnReport.Name = "btnReport";
+            this.btnReport.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnReport.Size = new System.Drawing.Size(192, 82);
+            this.btnReport.TabIndex = 3;
+            this.btnReport.Text = "Reporting";
+            this.btnReport.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnReport.UseVisualStyleBackColor = false;
+            this.btnReport.Click += new System.EventHandler(this.btnReport_Click);
+            // 
+            // btnSetup
+            // 
+            this.btnSetup.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSetup.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSetup.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSetup.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSetup.FlatAppearance.BorderSize = 0;
+            this.btnSetup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSetup.ForeColor = System.Drawing.Color.Black;
+            this.btnSetup.Image = ((System.Drawing.Image)(resources.GetObject("btnSetup.Image")));
+            this.btnSetup.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSetup.Location = new System.Drawing.Point(0, 190);
+            this.btnSetup.Name = "btnSetup";
+            this.btnSetup.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnSetup.Size = new System.Drawing.Size(192, 82);
+            this.btnSetup.TabIndex = 2;
+            this.btnSetup.Text = "SetUp";
+            this.btnSetup.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSetup.UseVisualStyleBackColor = false;
+            this.btnSetup.Click += new System.EventHandler(this.btnSetup_Click);
+            // 
+            // btnPOS
+            // 
+            this.btnPOS.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnPOS.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPOS.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPOS.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPOS.FlatAppearance.BorderSize = 0;
+            this.btnPOS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPOS.ForeColor = System.Drawing.Color.Black;
+            this.btnPOS.Image = ((System.Drawing.Image)(resources.GetObject("btnPOS.Image")));
+            this.btnPOS.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPOS.Location = new System.Drawing.Point(0, 108);
+            this.btnPOS.Name = "btnPOS";
+            this.btnPOS.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnPOS.Size = new System.Drawing.Size(192, 82);
+            this.btnPOS.TabIndex = 1;
+            this.btnPOS.Text = "POS";
+            this.btnPOS.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnPOS.UseVisualStyleBackColor = false;
+            this.btnPOS.Click += new System.EventHandler(this.btnPOS_Click);
             // 
             // pnlLogo
             // 
             this.pnlLogo.BackColor = System.Drawing.Color.DimGray;
+            this.pnlLogo.Controls.Add(this.pictureBox1);
             this.pnlLogo.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlLogo.Location = new System.Drawing.Point(0, 0);
             this.pnlLogo.Name = "pnlLogo";
-            this.pnlLogo.Size = new System.Drawing.Size(200, 90);
+            this.pnlLogo.Size = new System.Drawing.Size(192, 108);
             this.pnlLogo.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(192, 108);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pnlTitl
+            // 
+            this.pnlTitl.BackColor = System.Drawing.Color.White;
+            this.pnlTitl.Controls.Add(this.linkLabel1);
+            this.pnlTitl.Controls.Add(this.label1);
+            this.pnlTitl.Controls.Add(this.lblItem);
+            this.pnlTitl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitl.Location = new System.Drawing.Point(192, 0);
+            this.pnlTitl.Name = "pnlTitl";
+            this.pnlTitl.Size = new System.Drawing.Size(861, 108);
+            this.pnlTitl.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(3, 64);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(94, 25);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Migasoft";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(590, 9);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(48, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "POS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblItem
+            // 
+            this.lblItem.AutoSize = true;
+            this.lblItem.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblItem.ForeColor = System.Drawing.Color.White;
+            this.lblItem.Location = new System.Drawing.Point(39, 9);
+            this.lblItem.Name = "lblItem";
+            this.lblItem.Size = new System.Drawing.Size(52, 24);
+            this.lblItem.TabIndex = 0;
+            this.lblItem.Text = "POS";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(192, 108);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(861, 484);
+            this.pnlMain.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1053, 592);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.pnlTitl);
             this.Controls.Add(this.pnlMenu);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.pnlMenu.ResumeLayout(false);
+            this.pnlLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlTitl.ResumeLayout(false);
+            this.pnlTitl.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -69,6 +256,17 @@
 
         private System.Windows.Forms.Panel pnlMenu;
         private System.Windows.Forms.Panel pnlLogo;
+        private System.Windows.Forms.Button btnPOS;
+        private System.Windows.Forms.Button btnReport;
+        private System.Windows.Forms.Button btnSetup;
+        private System.Windows.Forms.Button btnOption;
+        private System.Windows.Forms.Panel pnlTitl;
+        private System.Windows.Forms.Label lblItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Panel pnlMain;
     }
 }
 
--- Resturant/Forms/Form1.cs
+++ Resturant/Forms/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -11,10 +12,136 @@ using System.Windows.Forms;
 namespace Resturant
 {
     public partial class Form1 : Form
-    {
+    {   
+        private Button currentButton;
+        private Form activeForm;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void OpenChildform(Form cForm,object btnSender)
+        {
+            if (activeForm!=null)
+            {
+                activeForm.Close();
+            }
+            ActiveButton(btnSender);
+            cForm.TopLevel = false;
+            cForm.FormBorderStyle= FormBorderStyle.None;
+            cForm.Dock= DockStyle.Fill;
+            pnlMain.Container.Add(cForm);
+            pnlMain.Tag= cForm;
+            cForm.BringToFront();
+            cForm.Show();
+
+        }
+        private Color SelectThems()
+        {
+            if (currentButton.Text == "POS")
+            {
+                return Color.Green;
+                
+
+            }
+            else if (currentButton.Text == "SetUp")
+            {
+                return Color.Brown;
+            }
+            else if (currentButton.Text == "Reporting")
+            {
+                return Color.Blue;
+            }
+            else if(currentButton.Text == "Options")
+            {
+                return Color.Red;
+            }
+            else
+            {
+                return Color.Gray;
+            }
+        }
+        private void ActiveButton(object sender) 
+        {
+            if (sender != null)
+            {
+                if (currentButton != (Button)sender )
+                {
+                    UnselectButton();
+                    currentButton=(Button)sender;
+                    Color color = SelectThems();
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new Font("Tahoma", 11F, FontStyle.Bold);
+                    currentButton.Size = new System.Drawing.Size(192, 150);
+                    pnlTitl.BackColor= color;
+                    lblItem.Text=currentButton.Text;
+                   
+
+                }
+            }
+        }
+        private void UnselectButton()
+        {
+            foreach (Control ctr in pnlMenu.Controls)
+            {
+                if (ctr.GetType()==typeof(Button))
+                {
+                   
+                    ctr.BackColor = Color.White;
+                    ctr.ForeColor = Color.Black;
+                    ctr.Font = new Font("Tahoma", 8F, FontStyle.Regular);
+                    ctr.Size = new System.Drawing.Size(192, 82);
+
+                }
+            }
+        }
+
+        private void btnPOS_Click(object sender, EventArgs e)
+        {
+            ActiveButton(sender);
+        }
+
+        private void btnSetup_Click(object sender, EventArgs e)
+        {
+            ActiveButton(sender);
+
+        }
+
+        private void btnReport_Click(object sender, EventArgs e)
+        {
+            ActiveButton(sender);
+
+        }
+
+        private void btnOption_Click(object sender, EventArgs e)
+        {
+            ActiveButton(sender);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://migawatt.com/");
         }
     }
 }
